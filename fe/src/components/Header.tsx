52f6e6e9101d0d58583aa8f178b653f371f32869
pragith98 <lakshanxp@hotmail.com>
2025-07-19 12:29:12 +0530

    feat: Create header

---
+++ fe/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <nav className="bg-gray-800 py-2">
+      <h1 className="text-3xl text-white font-extrabold text-center">
+        TODO APP
+      </h1>
+    </nav>
+  );
+}
+
+export default Header;
